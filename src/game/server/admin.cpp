14097342ac6d156e8bf35945b0c3f86d6d28d2e2
Myr-44 <85067522+Myr-13@users.noreply.github.com>
2022-06-29 02:14:10 +0500

    Admin menu based

---
+++ src/game/server/admin.cpp
@@ -0,0 +1,11 @@
+#include "admin.h"
+
+CAdmin::CAdmin(CGameContext *pGameServer)
+{
+	m_pGameServer = pGameServer;
+}
+
+void CAdmin::OnMenuAction(int ClientID, const char *pMsg)
+{
+	
+}
--- src/game/server/admin.h
+++ src/game/server/admin.h
@@ -4,16 +4,46 @@
 #include "game/server/gamecontext.h"
 #include "engine/server.h"
 
+enum {
+	ACTION_SET_HEALTH,
+	ACTION_SET_ARMOR,
+	ACTION_RAINBOW,
+	ACTION_BLOODY,
+	ACTION_GIVEGOLD,
+	ACTION_ENCHANTARMOR,
+	ACTION_RENAME,
+	ACTION_CHANGE_SKIN,
+	ACTION_GIVEDONATE,
+	ACTION_BRING,
+	ACTION_RETURN,
+	ACTION_KILL,
+	ACTION_GIVEUPGRADE,
+	ACTION_GIVESKILLPOINT,
+	ACTION_ANTIHOOK,
+	ACTION_ANTIDMG,
+	ACTION_KICK,
+	ACTION_MUTE,
+	ACTION_SETTITLE,
+	ACTION_GIVELEVEL,
+	ACTION_GIVEEXP
+};
+
+enum {
+	ADM_PLAYER
+};
+
 class CAdmin
 {
+	CGameContext* m_pGameServer;
+
 public:
 	CAdmin(CGameContext* pGameServer);
 
-private:
-	CGameContext* m_pGameServer;
-	CGameContext* GameServer() const { return m_pGameServer; }
+	void OnMenuAction(int ClientID, const char *pMsg);
+	void BuildVoteMenu(int ClientID);
 
-	void LastChat();
+	CGameContext* GameServer() const { return m_pGameServer; }
+	IServer* Server() const { return m_pGameServer->Server(); }
 };
 
 #endif
